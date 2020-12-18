8243a7ba8d897a0fb4191e2c9d1e6dbd821ec1d7
Daniela <dls_daniela@live.com>
2020-12-18 00:22:39 -0300

    criando banco de dados

---
+++ models/qrcode.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Qrcode extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Qrcode.belongsTo(models.Responsavel);
+      Qrcode.hasOne(models.Crianca);
+    }
+  };
+  Qrcode.init({
+    codigoQrcode: DataTypes.STRING,
+    localizacaoQrcode: DataTypes.STRING,
+    responsavelId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Qrcode',
+  });
+  return Qrcode;
+};
